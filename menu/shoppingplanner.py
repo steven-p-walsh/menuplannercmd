70e5cc48fe7b62ace38a2ca584a5f12bdedb3ef5
Steven Walsh <stevenpwalsh@gmail.com>
2019-10-13 10:06:35 -0400

    formatting of the output

--- menu/shoppingplanner.py
+++ menu/shoppingplanner.py
@@ -1,4 +1,5 @@
 from menu.utils import get_menu_ingredients, color
+from functools import reduce
 
 class ShoppingPlanner(object):
 
@@ -7,7 +8,7 @@ class ShoppingPlanner(object):
         self.mappings = mappings
         self.pantry = [ item['name'] for item in pantry ]
 
-    def __pick_mappings__(self):
+    def ingredient_groups(self):
         items = self.menu['items']
         # get distinct list of mappings
         menu_ingredients = get_menu_ingredients(items)
@@ -26,9 +27,37 @@ class ShoppingPlanner(object):
         # return the groups
         return groups
     
+    def print_columns(self, headers, columns):
+        max_length = max([ len(x) for x in columns ])
+        rows = []
+
+        # pivot the data, and fill in gaps
+        for i in range(0, max_length):
+            row = []
+            for col in columns:
+                value = col[i] if i < len(col) else ''
+                row.append(value)
+            rows.append(row)
+        
+        # build the format string
+        format_str = ''
+        for col in columns:
+            format_str = format_str + '{:<20s}'
+        
+        # print headers
+        print(color.BOLD + format_str.format(*headers) + color.END)
+
+        # print body
+        for row in rows:
+            print(color.CYAN + format_str.format(*row) + color.END)
+            
+
     def print_list(self):
-        mappings = self.__pick_mappings__()
+        mappings = self.ingredient_groups()
         groups = mappings.keys()
+
+        columns = []
+        headers = []
 
         for group in groups:
             ingredients = mappings[group]
@@ -37,10 +66,9 @@ class ShoppingPlanner(object):
             if len(ingredients) == 0:
                 continue
 
-            # print the list
-            print(color.BOLD + group + color.END)
-            for ingredient in ingredients:
-                ingredient_name = ingredient['name']
-                print(color.CYAN + ingredient_name + color.END)
+            headers.append(group)
+            columns.append([ing['name'] for ing in ingredients])
+
+        self.print_columns(headers, columns)
                 
             
